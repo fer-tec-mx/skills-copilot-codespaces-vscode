9f76cadc4d1c3cc0f4115e5d367d2890e502fad3
Fernando Rendón Bernal <112877708+fer-tec-mx@users.noreply.github.com>
2023-10-16 16:22:53 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,10 @@
 // Create a web server
+
+// Import the express module
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+// Handle the GET request for getting all comments
+router.get('/', commentController.getAllComments);
+
